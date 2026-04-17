4c0a608f2fb94fa2f09aa5e6c9361421c5c951e3
sulemaniftikhar <sulemanbhatti1810@gmail.com>
2026-04-17 14:57:02 +0500

    update .env.example

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
